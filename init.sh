600ae2f746c93063cc68855086a0946eae3fddc0
Derek Bourgeois <derek@ibourgeois.com>
2015-11-27 03:21:15 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,42 +1,39 @@
 #!/bin/bash
 
+# Add Repository Keys
+    # Arc Theme
+    wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_15.04/Release.key
+    sudo apt-key add - < Release.key
+
+# Add Repositories
+    # Arc Theme
+    sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_15.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
+    # Numix Theme
+    sudo add-apt-repository -y ppa:numix/ppa
+    
+# Update && Upgrade
+sudo apt-get update && sudo apt-get upgrade -y
+
 # Install Arc Theme
 echo '|========== Installing Arc Theme ==========|'
-
-wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_15.04/Release.key
-sudo apt-key add - < Release.key
-
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_15.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
-sudo apt-get update
 sudo apt-get install -y arc-theme
-
 echo '|========== Arc Theme Installed ==========|'
 echo '|========== Activating Arc Theme ==========|'
-
 gsettings set org.gnome.desktop.interface gtk-theme "Arc-Darker"
 gsettings set org.gnome.desktop.wm.preferences theme "Arc-Darker"
-
 echo '|========== Arc Theme Activated ==========|'
 
 # Install Numix Icons
 echo '|========== Installing Numix Icons ==========|'
-
-sudo add-apt-repository ppa:numix/ppa
-sudo apt-get update
 sudo apt-get install -y numix-icon-theme-circle
-
 echo '|========== Numix Icons Installed ==========|'
 echo '|========== Activating Numix Icons ==========|'
-
 gsettings set org.gnome.desktop.interface icon-theme "Numix-Circle"
-
 echo '|========== Numix Icons Activated ==========|'
 
 # Install Git
 echo '|========== Installing Git ==========|'
-
 sudo apt-get install -y git
-
 echo '|========== Git Installed ==========|'
 
 # Install Sublime Text
@@ -55,9 +52,7 @@ echo '|========== Git Installed ==========|'
 
 # Install Firefox
 echo '|========== Installing FireFox ==========|'
-
-sudo apt-get install -y firefox firefox-launchpad-plugin 
-
+sudo apt-get install -y firefox firefox-launchpad-plugin
 echo '|========== FireFox Installed ==========|'
 
 # Install FileZilla
@@ -80,7 +75,6 @@ echo '|========== FireFox Installed ==========|'
 
 # Build Bash Profile
 echo '|========== Building Bash Profile ==========|'
-
 echo "alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
@@ -124,5 +118,4 @@ extract () {
         echo \"'\$1' is not a valid file!\"
     fi
 }" > ~/.bash_aliases
-
 echo '|========== Bash Profile Built ==========|'
