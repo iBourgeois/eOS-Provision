c47ff36b3fe8aa025aeb4e57e6a6fcd7ae8049a8
Derek Bourgeois <derek@ibourgeois.com>
2015-11-27 01:12:32 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 # Install Arc Theme
+echo '|========== Installing Arc Theme ==========|'
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_15.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
+sudo apt-get update
+sudo apt-get install arc-theme
+wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_15.04/Release.key
+sudo apt-key add - < Release.key 
+echo '|========== Arc Theme Installed ==========|'
 
 # Install Numix Icons
+echo '|========== Installing Numix Icons ==========|'
+
+echo '|========== Numix Icons Installed ==========|'
 
 # Install Sublime Text
 
