46379839cf79305ad062900539b3f84d00837409
Derek Bourgeois <derek@ibourgeois.com>
2015-11-27 01:02:55 -0500

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Install Arc Theme
+
+# Install Numix Icons
+
+# Install Sublime Text
+
+# Install Atom
+
+# Install Docker
+
+# Install VirtualBox
+
+# Install Vagrant
+
+# Install VisualStudio Code
+
+# Install Google Chrome
+
+# Install Firefox
+
+# Install FileZilla
+
+# Install MySQL Workbench
+
+# Install Minetest
+
+# Install Google Music Manager
+
+# Install Skype
+
+# Install TeamViewer
+
+# Install BleachBit
+
+# Install WireShark
+
+# Install VLC
